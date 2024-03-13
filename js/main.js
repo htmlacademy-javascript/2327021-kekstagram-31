5ee9cab426a5e7eea26037be46a806afd480c140
Paul_Yatsenko <loyer86@mail.ru>
2024-03-13 18:35:43 +0300

    4.16. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
-const ID_ARR = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const DESCRIPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-const URL_ARR = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
 
-const DESCRIPTIONS_ALL = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+const AVATARS = [1, 2, 3, 4, 5, 6];
 
-const MESSAGE_ARR = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+const NAMES = ['Пауэр', 'Блек', 'Бульбозавр', 'Грюм', 'Миг29СМТ', 'Патрисия', 'Фитжеральд', 'Магос', 'Император человечества', 'Эйзенхорн', 'Джек Ричер', 'Доктор Слу', 'Гендальф', 'Нагибатор367', 'Магнето', 'Доминик Торетто', 'Лиза дочь маркиза', 'Тирекс', 'СиДаблюЭй', 'Снупи', 'Мазафакер', 'Шабалин', 'Эндрю 8 фунтов', 'Валера штанга', 'Зенит чемпион', 'Твой батя'];
 
-const AVATAR_ARR = [1, 2, 3, 4, 5, 6];
+const createUniqueIDComments = getRandomIntNoRepeat(1, 1000);
 
-const NAME_ARR = ['Пауэр', 'Блек', 'Бульбозавр', 'Грюм', 'Миг29СМТ', 'Патрисия', 'Фитжеральд', 'Магос', 'Император человечества', 'Эйзенхорн', 'Джек Ричер', 'Доктор Слу', 'Гендальф', 'Нагибатор367', 'Магнето', 'Доминик Торетто', 'Лиза дочь маркиза', 'Тирекс', 'СиДаблюЭй', 'Снупи', 'Мазафакер', 'Шабалин', 'Эндрю 8 фунтов', 'Валера штанга', 'Зенит чемпион', 'Твой батя'];
-
-const COMMENTS = () => {
+const getComments = () => {
   return {
-    id: getRandomIntNoRepeat(1, 1000),
-    avatar: 'img/avatar-' + getRandomElement(AVATAR_ARR),
-    message: getRandomElement(MESSAGE_ARR),
-    name: getRandomElement(NAME_ARR),
+    id: createUniqueIDComments(),
+    avatar: 'img/avatar-' + getRandomElement(AVATARS),
+    message: getRandomElement(MESSAGES),
+    name: getRandomElement(NAMES),
   }
 }
 
@@ -34,38 +32,34 @@ const getRandomElement = (elements) => {
   return elements[getRandomInteger(0, elements.length - 1)];
 };
 
-const getRandomIntNoRepeat = (min, max) => {
-  let targetArray = [];
+function getRandomIntNoRepeat(min, max) {
+  const previousValues = [];
 
   return function () {
     let currentValue = getRandomInteger(min, max);
-
-    if (targetArray.length >= (max - min + 1)) {
-      console.error('превышение кол-ва возможных значений');
+    if (previousValues.length >= (max - min + 1)) {
       return null;
     }
-
-    while (targetArray.includes(currentValue)) {
+    while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
     }
-    targetArray.push(currentValue);
+    previousValues.push(currentValue);
     return currentValue;
   };
 }
 
-const getRandomNoRepeatElement = (elements) => {
-  return elements[getRandomIntNoRepeat(0, elements.length - 1)];
-}
+const createUniqueID = getRandomIntNoRepeat(1, 25);
+const createUniqueUrl = getRandomIntNoRepeat(1, 25);
 
-const COMMENTS_All = Array.from({ length: getRandomInteger(1, 30) }, COMMENTS);
+const createComments = () => Array.from({ length: getRandomInteger(1, 30) }, getComments);
 
 const photoDescription = () => {
   return {
-    id: getRandomElement(ID_ARR),
-    url: 'photo/' + getRandomNoRepeatElement(URL_ARR) + '.jpg',
-    description: 'описание фото' + getRandomNoRepeatElement(DESCRIPTIONS_ALL),
+    id: createUniqueID(),
+    url: 'photo/' + createUniqueUrl() + '.jpg',
+    description: 'описание фото' + getRandomElement(DESCRIPTIONS),
     likes: getRandomInteger(15, 200),
-    comments: COMMENTS_All
+    comments: createComments()
   };
 }
 
